799be3cf45b55d1d74dd106700262a132595f212
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2024-01-18 13:10:28 +0530

    Setup datadog configs

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,4 +1,13 @@
 import tracer from "dd-trace";
 
-tracer.init();
+tracer.init({
+    hostname: `${process.env.DATADOG_AGENT_HOST}`,
+    port: `${process.env.DATADOG_AGENT_PORT}`,
+    env: "dev",
+    service: "simple-api",
+    logInjection: true,
+    tags: {
+        "simple-api.version": "1.0.0",
+    },
+});
 export default tracer;
