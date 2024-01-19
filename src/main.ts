a0d1d08d4a91da8725096d30b0cec6fa45f1558d
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2024-01-19 13:52:02 +0530

    Set datadog logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+import './tracer';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import './tracer';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
--- src/tracer.ts
+++ src/tracer.ts
@@ -1,6 +1,6 @@
-import tracer from "dd-trace";
+import tracer, { TracerOptions } from "dd-trace";
 
-tracer.init({
+const config: TracerOptions = {
     hostname: process.env.DATADOG_AGENT_HOST,
     port: process.env.DATADOG_AGENT_PORT,
     logInjection: process.env.DATA_DOG_LOG_INJECTION === "true",
@@ -9,5 +9,13 @@ tracer.init({
         hostname: process.env.DATADOG_AGENTD_HOST,
         port: Number(process.env.DATADOG_AGENTD_PORT),
     },
-});
+    logger: {
+        error: (msg) => console.error("DataDog: ", msg),
+        warn: (msg) => console.warn("DataDog: ", msg),
+        info: (msg) => console.info("DataDog: ", msg),
+        debug: (msg) => console.log("DataDog: ", msg),
+    },
+}
+console.log(config);
+tracer.init(config);
 export default tracer;
