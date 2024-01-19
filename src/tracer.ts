6d45862dc2462fc4b15721c6b5e6b1fbc42247ea
Hansa Jayathilaka <hansajayathilaka@yahoo.com>
2024-01-19 12:54:36 +0530

    Setup env file

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,13 +1,13 @@
 import tracer from "dd-trace";
 
 tracer.init({
-    hostname: `${process.env.DATADOG_AGENT_HOST}`,
-    port: `${process.env.DATADOG_AGENT_PORT}`,
-    env: "dev",
-    service: "simple-api",
-    logInjection: true,
-    tags: {
-        "simple-api.version": "1.0.0",
+    hostname: process.env.DATADOG_AGENT_HOST,
+    port: process.env.DATADOG_AGENT_PORT,
+    logInjection: process.env.DATA_DOG_LOG_INJECTION === "true",
+    runtimeMetrics: process.env.DATA_DOG_RUNTIME_METRICS === "true",
+    dogstatsd: {
+        hostname: process.env.DATADOG_AGENTD_HOST,
+        port: Number(process.env.DATADOG_AGENTD_PORT),
     },
 });
 export default tracer;
